8167f9a3a520c53cb1cfefdc764fd541fc1bbfda
RefoxFX <doffel24@mail.ru>
2017-05-12 16:15:14 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+jQuery('document').ready(function(){
+       jQuery('#info').click(function(){
+           
+    var menu_visible = jQuery('.info_list');
+    menu_visible.toggleClass('info_list_visible');
+                               });                   
+});

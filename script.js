e2141f32675fab4821d97466cf40cc553a4a82d1
SkywalterDBZ <klinokilampa@gmail.com>
2017-05-16 15:22:55 +0300

    edit_header

--- script.js
+++ script.js
@@ -6,7 +6,16 @@ $('document').ready(function(){
     $('#triangle').toggleClass('info_triangle_light');
     $('#info').toggleClass('info_hover');
                                });
-});
-$(window).resize(function(){
-    var window_size = $(window).width();
-});
+	$(document).mouseup(function (e){
+        if($('.info_list').hasClass('info_list_visible')){
+		var div = $("#info"); 
+		if (!div.is(e.target)
+		    && div.has(e.target).length  === 0) {
+			$('.info_list').toggleClass('info_list_visible');
+            $('#info').toggleClass('info_light');
+            $('#info span').toggleClass('info_span_light');
+            $('#triangle').toggleClass('info_triangle_light');
+            $('#info').toggleClass('info_hover');
+		};};
+	});
+}); 
